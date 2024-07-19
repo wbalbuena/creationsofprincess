2bf9b1461752b988a2cd6b203645891cafcc094e
wbalbuena <w_balbuena@u.pacific.edu>
2024-07-19 14:26:24 -0700

    Adjusted app.py code to use MySQL database on PythonAnywhere.  Note: Database is unable to be accessed locally now

--- src/app.py
+++ src/app.py
@@ -2,11 +2,26 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='templates', static_folder='css')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+# "mysql+mysqldb://{username}:{password}@{hostname}/{databasename}"
+SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://wbalbuena:NTC7cur6jvg*gzd-rxg@wbalbuena.mysql.pythonanywhere-services.com/wbalbuena$default"
 
-from database import init_db, Product
-db = init_db(app)
+app = Flask(__name__, template_folder='templates', static_folder='css')
+
+# pool_recycle - how long a database connection can be left unused before discarded
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle':280}
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.String(100), nullable=False)
+    dimensions = db.Column(db.String(100), nullable=True)
+    category = db.Column(db.String(100), nullable=False)
+    image_path = db.Column(db.String(100), nullable=False)
 
 @app.route("/")
 def index():
@@ -14,8 +29,7 @@ def index():
 
 @app.route("/products")
 def products():
-    products = Product.query.all()
-    return render_template('products.html', products=products)
+    return render_template('products.html', products=Product.query.all())
 
 @app.route("/contact")
 def contact():
